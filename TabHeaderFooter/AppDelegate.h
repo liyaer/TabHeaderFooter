ae6743edd10900f4ae0160687ceaff432408102b
DuWenLiang <739285514@qq.com>
2017-09-14 13:34:44 +0800

    Initial Commit

---
+++ TabHeaderFooter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabHeaderFooter
+//
+//  Created by 杜文亮 on 2017/9/14.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabHeaderFooter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabHeaderFooter
+//
+//  Created by 杜文亮 on 2017/9/14.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
